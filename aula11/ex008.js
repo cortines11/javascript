0c4c06e17be7475cde2edb0dd2c641c2c33020f4
Gabriel Cortines <166562067+cortines11@users.noreply.github.com>
2024-05-13 22:46:43 -0400

    1

---
+++ aula11/ex008.js
@@ -0,0 +1,12 @@
+var vel = 70
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if(vel >60) {
+    console.log(`Dirika sempre usando velocidade permitida. MULTADO!`)
+}
+
+
+
+
+
+
+console.log(`Dirija sempre usando cinto de segurança!`)
