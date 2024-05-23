460d412e5cdfee7ce515f318d80c165153961aec
Gabriel Cortines <166562067+cortines11@users.noreply.github.com>
2024-05-23 17:46:09 -0400

    1

---
+++ aulas/aula11/ex0081.js
@@ -0,0 +1,7 @@
+var vel = 150
+console.log (`A velocidade do carro é ${vel} km/h`)
+if(vel >100) {
+    console.log('Voce está acima da velocidade permitida, MULTADO!')
+} else {
+    console.log(`Sua velocidade atual é de ${vel}km/h, e está dentro da velocidade permitida!`)
+}
