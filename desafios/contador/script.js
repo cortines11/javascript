4bfdc74826571c43571b97a12b419d9d1a214ff9
Gabriel Cortines <166562067+cortines11@users.noreply.github.com>
2024-06-05 22:39:15 -0400

    1

--- desafios/contador/script.js
+++ desafios/contador/script.js
@@ -1,16 +1,16 @@
 function contar(){
-    var ini = document.getElementById('txti')
-    var fim = document.getElementById('txtn')
-    var passo = document.getElementById('txtp')
+    var ini = document.getElementById('txtn')
+    var fim = document.getElementById('txtf')
+    var pas = document.getElementById('txtp')
     var res = document.getElementById('res')
 
     if (ini.value.length ==0 || fim.value.length == 0 || pas.value.length ==0) {
         alert('ERRO! Faltam dados!')
     } else {
-        res.innerHTML += 'Contando:'
+        res.innerHTML = 'Contando:'
         var i = Number(ini.value)
         var f = Number(fim.value)
-        var p = Number(passo.value)
+        var p = Number(pas.value)
         
         if(i < f) {
             //contagem crescente
